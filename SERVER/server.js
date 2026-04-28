b0826a20aea0be193199fbad24ba21b2bcb35edb
ei-sanu <someshranjanbiswal13678@gmail.com>
2026-04-28 23:14:44 +0530

    added the payment verification

--- SERVER/server.js
+++ SERVER/server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 const app = express();
 
@@ -24,7 +25,7 @@ const orderRoutes = require("./routes/orderRoutes");
 
 
 // ✅ ROUTES USE
-app.use("/api/auth", authRoutes);
+app.use("/api/", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
@@ -41,7 +42,7 @@ app.get("/", (req, res) => {
 
 
 // ✅ SERVER START
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
   console.log(`🚀 Server started on port ${PORT}`);
